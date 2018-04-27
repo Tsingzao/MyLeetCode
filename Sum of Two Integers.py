c74246ad52b6235d8b20ec88ffe73c752c2f78e9
Tsingzao <yutingzhaomeng@hotmail.com>
2018-04-27 15:11:21 +0800

    Update Sum of Two Integers.py

--- Sum of Two Integers.py
+++ Sum of Two Integers.py
@@ -12,6 +12,20 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
+    
+        MAX = 0x7FFFFFFF
+        mask = 0xFFFFFFFF
+        while b != 0:
+            a, b = (a^b)&mask, ((a&b)<<1)&mask
+        return a if a <= MAX else ~(a^mask)
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
         if b == 0:
             return a
         else:
